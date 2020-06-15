d468b751ae31efca5fa9a02dff8a0328b0477d98
abdalhadi bakr <hade.bk.1998@gmail.com>
2020-06-15 22:44:13 +0200

    setup express server and firebase config

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -1,22 +1,47 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-admin.initializeApp();
-/*admin.initializeApp({
+admin.initializeApp({
     credential: admin.credential.applicationDefault()
-});*/
+});
 
 // init express server
 const express = require('express');
 const app = express();
 
+// firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyDenJlzVzL0LmDGNhZ6eH6TlZAmqFT4eJU",
+    authDomain: "twirrer-app.firebaseapp.com",
+    databaseURL: "https://twirrer-app.firebaseio.com",
+    projectId: "twirrer-app",
+    storageBucket: "twirrer-app.appspot.com",
+    messagingSenderId: "714072408637",
+    appId: "1:714072408637:web:eb709d07ebb91959a42054",
+    measurementId: "G-7B0TRRWSJW"
+};
+
+// firebase init
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig)
+
 // get all posts
 app.get('/getPosts', (req, res) =>{
-    admin.firestore().collection('posts').get()
+    admin.firestore().collection('posts').orderBy('createdAt', 'desc').get()
     .then(data =>{
         let posts=[];
         data.forEach(doc=>{
-            posts.push(doc.data());
+            posts.push({
+                userId: doc.data().userId,
+                userName: doc.data().userName,
+                userAvatar: doc.data().userAvatar,
+                postId: doc.id,
+                postContent: doc.data().postContent,
+                postImage: doc.data().postImage,
+                likeCount: doc.data().likeCount,
+                commentCount: doc.data().commentCount,
+                createdAt: doc.data().createdAt
+            });
         });
         return res.json(posts);
     })
@@ -24,9 +49,14 @@ app.get('/getPosts', (req, res) =>{
 })
 
 // add new post
-app.get('/addPost', (req, res) =>{
+app.post('/addPost', (req, res) =>{
     const post = {
-        name: req.body.name
+        userId: req.body.userId,
+        userName: req.body.userName,
+        userAvatar: req.body.userAvatar,
+        postContent: req.body.postContent,
+        postImage: req.body.postImage,
+        createdAt: new Date().toISOString()
     }
 
     admin.firestore().collection('posts').add(post)
@@ -40,4 +70,4 @@ app.get('/addPost', (req, res) =>{
 })
 
 // to tell firebase that app is the container of all routes
-exports.api = functions.https.onRequest(app);
+exports.api = functions.region('europe-west3').https.onRequest(app);
